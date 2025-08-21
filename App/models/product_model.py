a6e204b42885ae048b397580ce09b85f7a6f3abd
AllanPyDev <beckerallan925@gmail.com>
2025-08-21 11:17:21 -0300

    Update product_model.py

--- App/models/product_model.py
+++ App/models/product_model.py
@@ -1,1 +1,9 @@
+from datetime import datetime
 
+class Product:
+    def __init__(self, id, name, price, created_at=None, updated_at=None):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.created_at = created_at or datetime.now()
+        self.updated_at = updated_at or datetime.now()
