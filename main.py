c00c8fae36aee1980b3f9d43a7b143f29e9de9e9
AllanPyDev <beckerallan925@gmail.com>
2025-08-21 11:13:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from app.controllers.product_controller import ProductController
 
+controller = ProductController()
+
+# Criar produtos
+controller.create_product({"id": 1, "name": "Notebook", "price": 6000})
+controller.create_product({"id": 2, "name": "Mouse", "price": 3000})
+controller.create_product({"id": 3, "name": "Monitor", "price": 7500})
+
+# Atualizar produto
+print(controller.update_product(1, {"price": 6500}))
+
+# Filtrar produtos por preço
+filtered = controller.filter_products(5000, 8000)
+print("Produtos filtrados:", filtered)
