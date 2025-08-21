a83b75cd963fc1fdc30b8e8c2736625cbce59f32
AllanPyDev <beckerallan925@gmail.com>
2025-08-21 11:15:11 -0300

    Update test_product_schema.py

--- Tests/test_product_schema.py
+++ Tests/test_product_schema.py
@@ -1,1 +1,11 @@
+import pytest
+from app.schemas.product_schema import ProductSchema
+from pydantic import ValidationError
 
+def test_product_schema_valid():
+    product = ProductSchema(id=1, name="Notebook", price=6000)
+    assert product.price == 6000
+
+def test_product_schema_invalid_price():
+    with pytest.raises(ValidationError):
+        ProductSchema(id=2, name="Mouse", price=-10)
