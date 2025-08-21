c7199fd7df1830f938f5b2c7f2f635436753afe2
AllanPyDev <beckerallan925@gmail.com>
2025-08-21 11:17:56 -0300

    Update product_schema.py

--- App/schemas/product_schema.py
+++ App/schemas/product_schema.py
@@ -1,1 +1,12 @@
+from pydantic import BaseModel, validator
 
+class ProductSchema(BaseModel):
+    id: int
+    name: str
+    price: float
+
+    @validator("price")
+    def price_must_be_positive(cls, v):
+        if v <= 0:
+            raise ValueError("Price must be positive")
+        return v
